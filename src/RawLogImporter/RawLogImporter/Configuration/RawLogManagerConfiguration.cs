5479e5335d20748dcd54fcaf493fa0b6c4f48ca5
NightFish <nightfish.net@gmail.com>
2021-10-17 23:18:11 +1300

    Consolidate config classes.

--- src/RawLogImporter/RawLogImporter/Configuration/RawLogManagerConfiguration.cs
+++ src/RawLogImporter/RawLogImporter/Configuration/RawLogManagerConfiguration.cs
@@ -1,27 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 
 namespace RawLogManager.Configuration
 {
-    public class ApiConfiguration
+    public class RawLogManagerConfiguration
     {
+        public readonly TimeSpan importDelay;
         public readonly int rateLimitBurstCount;
         public readonly TimeSpan rateLimitInterval;
         public readonly TimeSpan rateLimitErrorDelay;
 
         /**
+         * <param name="importDelay">Time between an import finishing and the next one starting.</param>
          * <param name="rateLimitBurstCount">Number of requests that can be made before
          * encoutering the rate limiter.</param>
          * <param name="rateLimitErrorDelay">How often a request can be made on average.</param>
          * <param name="rateLimitInterval">How long to sleep for when the server returns
          * a HTTP 429 (too many requests) or the request times out.</param>
          */
-        public ApiConfiguration(int rateLimitBurstCount, TimeSpan rateLimitInterval, TimeSpan rateLimitErrorDelay)
+        public RawLogManagerConfiguration(
+            TimeSpan importDelay,
+            int rateLimitBurstCount,
+            TimeSpan rateLimitInterval,
+            TimeSpan rateLimitErrorDelay)
         {
+            this.importDelay = importDelay;
             this.rateLimitBurstCount = rateLimitBurstCount;
             this.rateLimitInterval = rateLimitInterval;
             this.rateLimitErrorDelay = rateLimitErrorDelay;
+        }
+
+        public static RawLogManagerConfiguration Build(IConfiguration configuration)
+        {
+            // TODO: Build configuration from IConfiguration object
+            return null;
         }
     }
 }
