8613595d3f880d8636b6704ed8372ea7ffc03080
NightFish <nightfish.net@gmail.com>
2021-10-18 17:45:30 +1300

    Implement policed logs.tf API client.

--- src/RawLogImporter/RawLogImporter/Remote/LogsTFApi/PolicedLogsTFApiClient.cs
+++ src/RawLogImporter/RawLogImporter/Remote/LogsTFApi/PolicedLogsTFApiClient.cs
@@ -4,20 +4,74 @@
 
 namespace LogChugger.Remote.LogsTFApi
 {
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Microsoft.Extensions.Logging;
 
     /// <summary>
     /// A rate-limited version of <see cref="LogsTFApi"/>.
     /// </summary>
-    internal class PolicedLogsTFApiClient : LogsTFApiClient
+    internal class PolicedLogsTFApiClient : LogsTFApiClient, IDisposable
     {
+        private readonly PolicedLogsTFApiClientSettings settings;
+        private readonly SemaphoreSlim requestTokenBucket;
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
         /// <summary>
         /// Initializes a new instance of the <see cref="PolicedLogsTFApiClient"/> class.
         /// </summary>
+        /// <param name="settings">Rate limiter settings for this policer.</param>
         /// <param name="loggerFactory">The logger factory to be used to create a new logger.</param>
-        public PolicedLogsTFApiClient(ILoggerFactory loggerFactory)
+        public PolicedLogsTFApiClient(PolicedLogsTFApiClientSettings settings, ILoggerFactory loggerFactory)
             : base(loggerFactory)
         {
+            this.settings = settings ?? throw new ArgumentNullException(nameof(settings));
+            this.requestTokenBucket = new SemaphoreSlim(settings.BurstRequestLimit, settings.BurstRequestLimit);
+            this.StartTokenLoopAsync();
+        }
+
+        /// <summary>
+        /// Gets latest log ID from the remote source that is not past the <paramref name="ignorePast"/> time
+        /// while adhering to rate limits specified in <see cref="PolicedLogsTFApiClientSettings"/>.
+        /// </summary>
+        /// <param name="ignorePast">Ignores any logs created past this date.</param>
+        /// <returns>Latest log ID.</returns>
+        public new async Task<int> GetLatestLogIDAsync(DateTime ignorePast)
+        {
+            await this.requestTokenBucket.WaitAsync();
+            return await base.GetLatestLogIDAsync(ignorePast);
+        }
+
+        /// <summary>
+        /// Gets log content from the remote source while adhering to
+        /// rate limits specified in <see cref="PolicedLogsTFApiClientSettings"/>.
+        /// </summary>
+        /// <param name="id">ID of the log to get.</param>
+        /// <returns>Raw log JSON string.</returns>
+        public new async Task<string> GetLogAsync(int id)
+        {
+            await this.requestTokenBucket.WaitAsync();
+            return await base.GetLogAsync(id);
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.cancellationTokenSource.Cancel();
+        }
+
+        /// <summary>
+        /// Starts a loop that releases one request token per cycle.
+        /// Cycle duration is specified in <see cref="PolicedLogsTFApiClientSettings"/>.
+        /// </summary>
+        private async void StartTokenLoopAsync()
+        {
+            while (!this.cancellationTokenSource.IsCancellationRequested)
+            {
+                await Task.Delay(this.settings.AverageRequestInterval, this.cancellationTokenSource.Token);
+                this.requestTokenBucket.Release();
+            }
         }
     }
 }
---
+++ src/RawLogImporter/RawLogImporter/Remote/LogsTFApi/PolicedLogsTFApiClientSettings.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="PolicedLogsTFApiClientSettings.cs" company="CIA">
+// Copyright (c) CIA. All rights reserved.
+// </copyright>
+
+namespace LogChugger.Remote.LogsTFApi
+{
+    using System;
+
+    /// <summary>
+    /// Rate limiter settings for <see cref="PolicedLogsTFApiClient"/>.
+    /// </summary>
+    internal class PolicedLogsTFApiClientSettings
+    {
+        /// <summary>
+        /// Gets or sets the number of requests that can be made before reaching the <see cref="AverageRequestInterval"/>.
+        /// </summary>
+        public int BurstRequestLimit { get; set; }
+
+        /// <summary>
+        /// Gets or sets the average interval between requests.
+        /// </summary>
+        public TimeSpan AverageRequestInterval { get; set; }
+    }
+}
