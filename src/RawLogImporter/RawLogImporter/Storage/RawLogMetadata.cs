4552f5e2057174e35c426f4384bf59cc65e5a69b
NightFish <nightfish.net@gmail.com>
2021-10-19 11:17:27 +1300

    Add object for data mapping from the database.

---
+++ src/RawLogImporter/RawLogImporter/Storage/RawLogMetadata.cs
@@ -0,0 +1,71 @@
+﻿// <copyright file="RawLogMetadata.cs" company="CIA">
+// Copyright (c) CIA. All rights reserved.
+// </copyright>
+
+namespace LogChugger.Storage
+{
+    using System;
+
+    /// <summary>
+    /// Represents a record in the raw log metadata repository.
+    /// </summary>
+    public class RawLogMetadata
+    {
+        /// <summary>
+        /// Import status of a log.
+        /// </summary>
+        public enum RawLogImportStatus
+        {
+            /// <summary>
+            /// Log is queued to be imported.
+            /// </summary>
+            ToImport,
+
+            /// <summary>
+            /// Log has been successfully imported.
+            /// </summary>
+            Succeed,
+
+            /// <summary>
+            /// Log import was attempted and failed.
+            /// </summary>
+            Failed,
+
+            /// <summary>
+            /// Log was not found on the remote log source.
+            /// </summary>
+            NotFound,
+        }
+
+        /// <summary>
+        /// Gets or sets the unique log ID.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current import status of this log.
+        /// </summary>
+        public RawLogImportStatus ImportStatus { get; set; }
+
+        /// <summary>
+        /// Gets or sets the failure message.
+        /// Populated when a log import status is set to failed.
+        /// </summary>
+        public string FailureMessage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hash of the log used for duplicate detection.
+        /// </summary>
+        public byte[] Hash { get; set; }
+
+        /// <summary>
+        /// Gets or sets shared ID for duplicate logs.
+        /// </summary>
+        public int? Duplicate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time that the log was added or updated.
+        /// </summary>
+        public DateTime Time { get; set; }
+    }
+}
