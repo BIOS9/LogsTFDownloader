8cdc51e8a48d1fc479d38642beb1a6ac138ef6ae
NightFish <nightfish.net@gmail.com>
2021-10-18 18:32:31 +1300

    Add methods for raw log repo.

--- src/RawLogImporter/RawLogImporter/Storage/DiskLogRepository.cs
+++ src/RawLogImporter/RawLogImporter/Storage/DiskLogRepository.cs
@@ -4,10 +4,67 @@
 
 namespace LogChugger.Storage
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+
     /// <summary>
     /// A local disk filesystem based storage repository for raw JSON logs.
     /// </summary>
     internal class DiskLogRepository : IRawLogRepository
     {
+        private readonly ILogger logger;
+        private readonly DiskLogRepositorySettings settings;
+        private readonly string logsDirectory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DiskLogRepository"/> class.
+        /// </summary>
+        /// <param name="loggerFactory">Logger factory used to create logger.</param>
+        /// <param name="settings">Repository settings.</param>
+        public DiskLogRepository(ILoggerFactory loggerFactory, DiskLogRepositorySettings settings)
+        {
+            this.logger = loggerFactory.CreateLogger(nameof(DiskLogRepository));
+            this.settings = settings ?? throw new ArgumentNullException(nameof(settings));
+            this.logsDirectory = Path.GetFullPath(settings.LogsDirectoryPath);
+            this.logger.LogInformation("Using path {path} for log storage", this.logsDirectory);
+        }
+
+        /// <inheritdoc/>
+        public Task DeleteLogAsync(int id)
+        {
+            this.logger.LogDebug("Deleting log {id}", id);
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task<bool> DoesLogExistAsync(int id)
+        {
+            this.logger.LogDebug("Checking if log exists {id}", id);
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task<ICollection<int>> GetAllLogIDsAsync()
+        {
+            this.logger.LogDebug("Getting all log IDs (this may take a while).");
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task<string> GetLogAsync(int id)
+        {
+            this.logger.LogDebug("Getting log content {id}", id);
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task SaveLogAsync(string content, int id)
+        {
+            this.logger.LogDebug("Saving log {id}", id);
+            throw new NotImplementedException();
+        }
     }
 }
--- src/RawLogImporter/RawLogImporter/Storage/IRawLogRepository.cs
+++ src/RawLogImporter/RawLogImporter/Storage/IRawLogRepository.cs
@@ -4,10 +4,47 @@
 
 namespace LogChugger.Storage
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
     /// <summary>
     /// A storage repository for raw JSON logs.
     /// </summary>
     public interface IRawLogRepository
     {
+        /// <summary>
+        /// Returns a boolean indicating whether the specified log ID exists in the repository.
+        /// </summary>
+        /// <param name="id">Log ID to search for.</param>
+        /// <returns>True if the specified log exists and false if it doesn't.</returns>
+        public Task<bool> DoesLogExistAsync(int id);
+
+        /// <summary>
+        /// Returns the content of the specified log.
+        /// </summary>
+        /// <param name="id">ID of the log to get.</param>
+        /// <returns>Text content of the log.</returns>
+        public Task<string> GetLogAsync(int id);
+
+        /// <summary>
+        /// Saves a log to the repository.
+        /// </summary>
+        /// <param name="content">Text content of the log.</param>
+        /// <param name="id">Unique ID of the log to save.</param>
+        /// <returns>Asynchronous task.</returns>
+        public Task SaveLogAsync(string content, int id);
+
+        /// <summary>
+        /// Deletes a log from the repository.
+        /// </summary>
+        /// <param name="id">Unique ID of the log to delete.</param>
+        /// <returns>Asynchronous task.</returns>
+        public Task DeleteLogAsync(int id);
+
+        /// <summary>
+        /// Returns the IDs of all logs present in the repository,
+        /// </summary>
+        /// <returns>A collection of log ID integers.</returns>
+        public Task<ICollection<int>> GetAllLogIDsAsync();
     }
 }
