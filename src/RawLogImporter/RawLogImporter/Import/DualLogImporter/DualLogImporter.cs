0f4695a6ab1169a8737f7ea4539f9b20a2d545a9
NightFish <nightfish.net@gmail.com>
2021-10-19 00:14:19 +1300

    Add dual log importer boilerplate.

---
+++ src/RawLogImporter/RawLogImporter/Import/DualLogImporter/DualLogImporter.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="DualLogImporter.cs" company="CIA">
+// Copyright (c) CIA. All rights reserved.
+// </copyright>
+
+namespace LogChugger.Import.DualLogImporter
+{
+    /// <summary>
+    /// Log importer that sources logs from both the local raw log repository
+    /// and the remote log source.
+    /// This allows bulk importing of existing logs without having to use
+    /// the remote log source.
+    /// </summary>
+    internal class DualLogImporter : IRawLogImporter
+    {
+    }
+}
