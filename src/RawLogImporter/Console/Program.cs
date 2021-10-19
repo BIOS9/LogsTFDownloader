4c3bf0c765cb7b2dba0a2840b656c1fb7c63fcb0
NightFish <nightfish.net@gmail.com>
2021-10-19 17:58:31 +1300

    Change project folder structure.

--- src/RawLogImporter/Console/Program.cs
+++ src/RawLogImporter/Console/Program.cs
@@ -17,7 +17,7 @@ namespace LogImporterExample
               .CreateLogger();
 
             IConfigurationRoot config = new ConfigurationBuilder()
-                .AddJsonFile("settings.dev.json")
+                .AddJsonFile("settings.json")
                 .Build();
 
             RawLogManager logManager = new RawLogManager(
